f9c5117529ce83077bdb7867c4364a360c4fbb7f
OoNEKERAFAoO <nekerafa@hotmail.com>
2011-06-25 06:18:53 -0700

    Update 1.01v. More info in README, What is new?

--- execute.sh
+++ execute.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-# Compile CreatePass 1.0 (Linux)
+# Compile CreatePass 1.01 (Linux)
 #
-# Create by NEKERAFA on thu, 22 jun 2011 00:20:31 (CEST)
-# Copyleft 2011 NEKERAFA (nekerafa@gmail.com)
+# Create by NEKERAFA on sat, 25 jun 2011 13:25:22 (CEST) 
+# Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Spain 2011 NEKERAFA (nekerafa@gmail.com)
+# http://creativecommons.org/licenses/by-nc-sa/3.0/es/legalcode.es
 
 # Variables
-VERSION=1.0
+VERSION=1.01
 
 # Loading CreatePass
 echo -e '\e[0;32mLoading CreatePass '$VERSION' (Linux)...'
@@ -13,26 +14,32 @@ echo -e '\e[0;32mLoading CreatePass '$VERSION' (Linux)...'
 # Detecting Language
 if [ $LANG="es_ES.UTF-8" ]; then
 	# Spanish
-	echo -e 'Español...\n'
+	echo 'Español...'
 	lang="es"
 else
 	# English
-	echo -e 'English...\n'
+	echo 'English...'
 	lang="en"
 fi
+
+# Print language
+echo -e '   '$LANG'\n'
 
 # Compile CreatePass
 if [ $lang="es" ]; then
 	# Compiling spanish file
-	echo -e 'Compilando...\e[0m'
-	gcc -Wall -o "CreatePass" "_$lang.c" || { echo -e '\e[0;32mERROR: No se ha podido compilar CreatePass.\n	Error en _'$lang', ¿Existe el archivo?\e[0m\n'; exit 1; }
-	echo -e '\e[0;32mCompilado.\nIniciando CreatePass...\n\n\e[0m'
+	echo 'Compilando...'
+	echo -e '   gcc -Wall -o "CreatePass" "_'$lang'.c"\e[0m'
+	gcc -Wall -o "CreatePass" "_$lang.c" || { echo -e '   \e[0;31mERROR: No se ha podido compilar CreatePass.\n   Error en _'$lang', ¿Existe el archivo?\e[0m\n'; exit 1; }
+	echo -e '\e[0;32mCompilado.\nIniciando CreatePass...\e[0m'
 else
 	# Compiling english file
 	echo -e 'Compiling...[0m'
-	gcc -Wall -o "CreatePass" "_$lang.c" || { echo -e '\e[0;32mERROR: Failed to compile CreatePass.Error in _'$lang'.c, Is there the file?\n\e[0m\n'; exit 1; }
-	echo -e '\e[0;32mCompiled.\nStarting CreatePass...\n\n\e[0m'
+	echo '   gcc -Wall -o "CreatePass" "_'$lang'.c"'
+	gcc -Wall -o "CreatePass" "_$lang.c" || { echo -e '   \e[0;31mERROR: Failed to compile CreatePass.\n   Error in _'$lang'.c, Is there the file?\n\e[0m\n'; exit 1; }
+	echo -e '	\e[0;32mCompiled.\nStarting CreatePass...\e[0m'
 fi
 
 # Starting CreatePass
+echo -e '   ./CreatePass\n\n'
 ./CreatePass
